99a69cd6f159bcb38450c6198734e74a8d6bdecf
Eric Halverson <archeryislife@gmail.com>
2021-01-18 14:35:58 -0700

    CleanUp

--- PasswordGen/main.go
+++ PasswordGen/main.go
@@ -27,9 +27,9 @@ func main() {
 		passwordLength := 60
 		password := GeneratePassword(passwordLength, minSpecialChar, minNum, minUpperCase, minLowerCase)
 		//Uncomment next line for DLP tracking tag
-		const dlp = "ENTER DLP STRING HERE"
+		//const dlp = "ENTER DLP STRING HERE"
 		//Make next line fmt.Println(dlp + password)
-		fmt.Println(dlp + password)
+		fmt.Println(password)
 		i++
 	}
 }
@@ -67,4 +67,3 @@ func GeneratePassword(passwordLength, minSpecialChar, minNum, minUpperCase, minL
 	})
 	return string(inRune)
 }
-//Commit
